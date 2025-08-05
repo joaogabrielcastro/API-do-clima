653bdcfeebf9cdf27c888556445c24e4aab6d118
João Wotekoski - E. Escolinha de TI - BNE <joaowotekoski@bne.com.br>
2025-08-05 14:32:32 -0300

    Escopo

---
+++ JS/script.js
@@ -0,0 +1,4 @@
+// key = 2caefbb54e101fa03849d72535dc3464
+
+// exemplo de chamada de API 
+//https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=2caefbb54e101fa03849d72535dc3464
